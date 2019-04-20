ebf6bff70d9cf31ec7350b518a5a6c92c328935c
Adam Ali <adam@Adams-MacBook-Pro.local>
2019-04-20 13:53:48 -0700

    added checks for 150 miles radius

--- source/weather.js
+++ source/weather.js
@@ -66,7 +66,19 @@ function makeCorsRequest(url) {
       let responseStr = xhr.responseText;  // get the JSON string 
       let object = JSON.parse(responseStr);  // turn it into an object
       /*This is where we will handle the response*/
-      console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
+
+      let davisLat = 38.5449;
+      let davisLon = -121.7405;
+      let desiredLocationLat = object["city"]["coord"]["lat"];
+      let desireLocationLon = object["city"]["coord"]["lon"];
+      //distance function checks if distance is less than or equals to 150 miles from Davis (our default location)
+      if (distance(davisLat, davisLon,desiredLocationLat,desireLocationLon, "M")) {
+        console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
+      } else { //distance is > 150 miles
+        //TODO - Handle this in some way. This is if a user doesn't enter location within 150 miles
+        console.log("Invalid location entered");
+      }
+     
   };
 
   xhr.onerror = function() {
@@ -75,5 +87,57 @@ function makeCorsRequest(url) {
 
   // Actually send request to server
   xhr.send();
+}
+
+//MODIFIED A BIT -- Returns bool (dist <= 150) instead of the original distance
+//Below is the source!
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at https://www.geodatasource.com                         :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: https://www.geodatasource.com                       :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2018            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist <= 150;
+	}
 }
 
